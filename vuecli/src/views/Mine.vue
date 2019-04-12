e27c47970cc22aaa07047bd826b9134fdcf64e0e
tanjiuyusila <521196748@qq,com>
2019-04-12 20:46:05 +0800

    vuecli

--- vuecli/src/views/Mine.vue
+++ vuecli/src/views/Mine.vue
@@ -1,8 +1,25 @@
 <template>
-  <h1>这是我的页面</h1>
+  <!--只有一个根元素-->
+  <div>
+    <h1>这是我的页面</h1>
+<!--例2：子路由-->
+    <!--<router-link to="/mine/test1">子路由1</router-link>|-->
+    <!--<router-link to="/mine/test2">子路由2</router-link>|-->
+    <!--&lt;!&ndash;是mine的子路由，就要在mine的template里写一个router-view&ndash;&gt;-->
+    <!--<router-view></router-view>-->
+<!---->
+<!--例3：参数传递 写成对象结构-->
+    <router-link :to="{name:'test1',params:{name:'yaozhilin',age:25}}">子路由1</router-link>|
+    <!--写成url传参之后，不加参数就不会好使了，路径/test2和test2/:name/:age不是一个路径-->
+    <router-link to="/mine/test2">子路由2</router-link>|
+    <router-view></router-view>
+<!---->
+  </div>
 </template>
 <script>
+  export default {
 
+  }
 </script>
 <style>
 
---
+++ vuecli/src/views/Test1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>子路由1</h1>
+    <!--params不可以随意命名-->
+    <h1>名字是：{{$route.params.name}}</h1>
+    <h1>年龄是：{{$route.params.age}}</h1>
+  </div>
+
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style>
+
+</style>
