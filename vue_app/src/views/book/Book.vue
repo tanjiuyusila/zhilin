cafb25c8d031ec1319a50a576e10185004365ff5
tanjiuyusila <521196748@qq,com>
2019-04-24 10:49:23 +0800

    vueapp

--- vue_app/src/views/book/Book.vue
+++ vue_app/src/views/book/Book.vue
@@ -1,17 +1,51 @@
 <template>
-  <div>书籍</div>
+  <div class="book">
+    <swipe class="my-swipe">
+      <swipe-item class="item1"></swipe-item>
+      <swipe-item class="item2"></swipe-item>
+      <swipe-item class="item3"></swipe-item>
+    </swipe>
+  </div>
 </template>
 <script>
+  import {Swipe,SwipeItem} from 'vue-swipe';
   export default{
-//Day2:解决刷新问题：
-//     created(){
-//       this.$store.dispatch('changeBg',{
-//         bgColor:'cornflowerblue',
-//         title:'book'
-//       })
-//     }
+    data(){
+      return {
+
+      }
+    },
+    components:{
+      'swipe':Swipe,
+      'swipe-item':SwipeItem
+    }
   }
 </script>
-<style>
+<style scoped>
 
+  .my-swipe {
+    height: 200px;
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+  }
+
+  .item1 {
+    /*路径要写成*/
+    background: url('http://localhost:8081/img/01.jpg') no-repeat center;
+    background-size: contain;
+    color: #fff;
+  }
+
+  .item2 {
+    background: url('http://localhost:8081/img/02.jpg') no-repeat center;
+    background-size: contain;
+    color: #000;
+  }
+
+  .item3 {
+    background: url('http://localhost:8081/img/03.jpg') no-repeat center;
+    background-size: contain;
+    color: #fff;
+  }
 </style>
